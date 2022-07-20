89d4531b23d49acc8ff31d3dc700bf66691883bd
Akhmadjon Rajabov <35695668+RajabovAhmadjon@users.noreply.github.com>
2022-07-20 22:45:56 +0500

    Update and rename Integer-to-Roman.md to integer-to-roman.py

--- challenges-new-users/integer-to-roman.py
+++ challenges-new-users/integer-to-roman.py
@@ -1,3 +1,4 @@
+"""
 ## Description
 Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
 
@@ -35,3 +36,12 @@ Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
  
 #### Constraints:
 1 <= num <= 3999
+"""
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        M = ["", "M", "MM", "MMM"]
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return M[num // 1000] + C[(num % 1000) // 100] + X[(num % 100) // 10] + I[num % 10]
